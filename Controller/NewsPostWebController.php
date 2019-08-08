55add13c0e015bb6be311cad791ce02c45a63391
danilpanshin <danilpanshin@gmail.com>
2019-08-08 19:21:15 +0300

    work

--- Controller/NewsPostWebController.php
+++ Controller/NewsPostWebController.php
@@ -13,7 +13,8 @@ class NewsPostWebController {
     private $cache;
     private $repository;
 
-    public function __construct(cacheInterface $cache) {
+    public function __construct(cacheInterface $cache) 
+    {
     /*
     * Получаем объект из слоя кэша, из которого будем получать данные
     * Инициализируем репозиторий, на случай если кэш пуст
@@ -25,10 +26,31 @@ class NewsPostWebController {
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/post/{id}", name="post")
      */
-    public function showNewsPostById($newsId) {
+    public function showNewsPostById($newsId) 
+    {
     /*
     * Получаем данные из тэгированного кэша  
     */
+    }
+
+    /*
+    *Пагинация
+    */
+    public function listAction(Request $request)
+    {
+        $em    = $this->get('doctrine.orm.entity_manager');
+        $dql   = "SELECT a FROM AcmeMainBundle:Post a";
+        $query = $em->createQuery($dql);
+
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        // parameters to template
+        return $this->render('post/list.html.twig', ['pagination' => $pagination]);
     }
 
 
